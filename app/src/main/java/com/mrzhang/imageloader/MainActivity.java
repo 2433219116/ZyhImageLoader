b82cd8e2b9bdd294b12de3b171ca8a469b1508c2
icebergtsn <zyh2433219116@gmail.com>
2019-09-03 11:26:41 +0800

    Update ui

--- app/src/main/java/com/mrzhang/imageloader/MainActivity.java
+++ app/src/main/java/com/mrzhang/imageloader/MainActivity.java
@@ -1,5 +1,6 @@
 package com.mrzhang.imageloader;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatButton;
@@ -9,13 +10,16 @@ import android.support.v7.widget.AppCompatTextView;
 import android.view.View;
 import android.widget.Toast;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
 
     AppCompatImageView mImgLoader;
-    AppCompatButton mBtnDisplay;
+    AppCompatButton mBtnDisplay, mBtnClear;
     AppCompatEditText mEvUrl;
-    String url;
-    String nativeUrl="https://i1.go2yd.com/image.php?url=0JkUDu6Qar";
+    String mGetUrl;
+    String nativeUrl = "https://i1.go2yd.com/image.php?url=0JkUDu6Qar";
 
     @Override
 
@@ -30,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
     private void initView() {
         mImgLoader = findViewById(R.id.image_loader);
         mBtnDisplay = findViewById(R.id.btn_display);
+        mBtnClear = findViewById(R.id.btn_clear);
         mEvUrl = findViewById(R.id.tv_url);
     }
 
@@ -37,13 +42,37 @@ public class MainActivity extends AppCompatActivity {
         mBtnDisplay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                url = mEvUrl.getText().toString().trim();
-                if (url.isEmpty()) {
+                if (mEvUrl.getText() == null) {
+                    Toast.makeText(MainActivity.this, "Url为空", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                mGetUrl = mEvUrl.getText().toString().trim();
+                if (mGetUrl.isEmpty()) {
                     Toast.makeText(MainActivity.this, "显示默认Url", Toast.LENGTH_SHORT).show();
                     ImageLoader loader = new ImageLoader();
                     // loader.displayImage(url, mImgLoader);
                     loader.displayImage(nativeUrl, mImgLoader);
+                } else {
+                    try {
+                        new URL(mGetUrl);
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                        Toast.makeText(MainActivity.this, "Url error", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+
+                    Toast.makeText(MainActivity.this, "显示填充Url", Toast.LENGTH_SHORT).show();
+                    ImageLoader loader = new ImageLoader();
+                    // loader.displayImage(url, mImgLoader);
+                    loader.displayImage(mGetUrl, mImgLoader);
                 }
+            }
+        });
+
+        mBtnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mEvUrl.setText("");
             }
         });
     }
